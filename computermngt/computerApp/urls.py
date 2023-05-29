6bf3e9bc94e4f46d0ee00c446ba495cfd45f0483
K3vinK <117437538+K3vinK@users.noreply.github.com>
2023-05-29 23:51:10 +0200

    Update urls.py

--- computermngt/computerApp/urls.py
+++ computermngt/computerApp/urls.py
@@ -2,19 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('machines/',
-     views.machine_list_view,
-     name='machines'),
-    path('machine/<pk>',
-     views.machine_detail_view,
-     name='machine-detail'),
 
-     path('personnes/',
-     views.personne_list_view,
-     name='personnes'),
-     path('personne/<st>', 
-     views.personne_detail_view,
-     name='personne-detail'),
+    path('', views.index, name='index'),
+    path('machines/', views.machine_list_view, name='machines'),
+    path('machine/<pk>', views.machine_detail_view, name='machine-detail'),
+    path('add-machine/', views.machine_add_form, name='add-machine'),
+
+    path('', views.index, name='index'),
+    path('personnes/', views.personne_list_view, name='personnes'),
+    path('personne/<pk>', views.personne_detail_view, name='personne-detail'),
+    path('add-personne/', views.person_add_form, name='add-personne'),
 
 ]
